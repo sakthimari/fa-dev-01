e64f5fdff6bbf41605af366fbb4e63edf2e93763
sakthimari <sakthimari@gmail.com>
2025-08-05 23:19:11 -0400

    added authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
+import { useAuthenticator } from '@aws-amplify/ui-react';
+
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+    const { signOut } = useAuthenticator();
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -33,6 +37,8 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
