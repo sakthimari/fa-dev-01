10bbe9030dc4927bf087c4dca6d56b2b1a8cdb70
sakthimari <sakthimari@gmail.com>
2025-08-16 13:12:16 -0400

    updated code fix

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,13 +3,15 @@ import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import { sendInvitation } from  './backend/functions/send-invitation/resource'; // add this
 // Removed sendInvitation import
 
 export const backend = defineBackend({
   auth,
   data,
   storage,
-  // Removed sendInvitation from backend definition
+  sendInvitation
+  
 });
 
 // Grant SES permissions to the sendInvitation function
