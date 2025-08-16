87a3db6d18d08b0532e79c7ec9f826d32fe37cca
sakthimari <sakthimari@gmail.com>
2025-08-16 13:54:00 -0400

    send email fix

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,14 +3,14 @@ import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-import { sendInvitation } from  './backend/functions/send-invitation/resource'; // add this
+//import { sendInvitation } from  './backend/functions/send-invitation/resource'; // add this
 // Removed sendInvitation import
 
 export const backend = defineBackend({
   auth,
   data,
   storage,
-  sendInvitation
+  //sendInvitation
   
 });
 
